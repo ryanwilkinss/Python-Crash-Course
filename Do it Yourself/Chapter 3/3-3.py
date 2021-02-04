9c91b8ce2b0bd0a0bdac879f26ff769431243f8a
ryanwilkinss <78103496+ryanwilkinss@users.noreply.github.com>
2021-02-04 09:41:22 -0800

    Create 3-3.py

---
+++ Do it Yourself/Chapter 3/3-3.py
@@ -0,0 +1,9 @@
+# 3-3. Your Own List: Think of your favorite mode of transportation, such as a
+# motorcycle or a car, and make a list that stores several examples. Use your list
+# to print a series of statements about these items, such as “I would like to own a
+# Honda motorcycle.”
+
+motorcycle_list = 'ducati', 'honda', 'yamaha'
+print(f'I would like to own a {motorcycle_list[0].title()} one day.')
+print(f'I would like to own a {motorcycle_list[1].title()} one day.')
+print(f'I would like to own a {motorcycle_list[2].title()} one day.')
