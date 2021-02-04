c6fcff5eca75fd80bcce99d314b8214325fd2dd8
ryanwilkinss <78103496+ryanwilkinss@users.noreply.github.com>
2021-02-04 08:21:56 -0800

    Delete 3-1.py

--- Do it Yourself/3-1.py
+++
@@ -1,5 +0,0 @@
-# 3-1. Names: Store the names of a few of your friends in a list called names. Print
-# each person’s name by accessing each element in the list, one at a time.
-
-names = ["Elliott", "Andreas", "Scotty"]
-print(names)
