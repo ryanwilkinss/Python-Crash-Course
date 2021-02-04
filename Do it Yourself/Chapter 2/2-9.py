ecd84a830f705d1ea990da330a845b2ff22285bd
ryanwilkinss <78103496+ryanwilkinss@users.noreply.github.com>
2021-02-03 20:25:25 -0800

    Create 2-9.py

---
+++ Do it Yourself/Chapter 2/2-9.py
@@ -0,0 +1,6 @@
+# 2-9. Favorite Number: Use a variable to represent your favorite number. Then,
+# using that variable, create a message that reveals your favorite number. Print
+# that message.
+
+favorite_number = 11
+print(f'My favorite number is, {favorite_number}.')
