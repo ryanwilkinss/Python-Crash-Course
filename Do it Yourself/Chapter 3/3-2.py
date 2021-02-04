985d055795bd71152e6836e2e7229ad203250b77
ryanwilkinss <78103496+ryanwilkinss@users.noreply.github.com>
2021-02-04 09:40:41 -0800

    Create 3-2.py

---
+++ Do it Yourself/Chapter 3/3-2.py
@@ -0,0 +1,9 @@
+# 3-2. Greetings: Start with the list you used in Exercise 3-1, but instead of just
+# printing each person’s name, print a message to them. The text of each message
+# should be the same, but each message should be personalized with the
+# person’s name.
+
+name = 'elliott', 'andreas', 'scotty'
+print(f'Hey {name[0].title()}, hows it going?')
+print(f'Hey {name[1].lower()}, hows it going?')
+print(f'Hey {name[2].upper()}, hows it going?')
