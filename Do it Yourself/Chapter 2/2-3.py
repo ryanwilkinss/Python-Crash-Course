2a70fa3a596081e84636657817d2761131bc64f9
ryanwilkinss <78103496+ryanwilkinss@users.noreply.github.com>
2021-02-03 19:52:46 -0800

    Create 2-3.py

---
+++ Do it Yourself/Chapter 2/2-3.py
@@ -0,0 +1,7 @@
+# 2-3. Personal Message: Use a variable to represent a person’s name, and print
+# a message to that person. Your message should be simple, such as, “Hello Ryan,
+# would you like to learn some Python today?”
+
+name = 'Ryan'
+message = f'Hello {name.title()}, would you like to learn some Python today?'
+print(message)
