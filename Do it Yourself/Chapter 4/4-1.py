933e4a28e2927d6afb883ebb4d587425ef743366
ryanwilkinss <78103496+ryanwilkinss@users.noreply.github.com>
2021-02-04 11:43:21 -0800

    Create 4-1.py

---
+++ Do it Yourself/Chapter 4/4-1.py
@@ -0,0 +1,13 @@
+# Think of at least three kinds of your favorite pizza. Store these pizza names
+# in a list, and then use a for loop to print the name of each pizza.
+favorite_pizzas = ['pepperoni', 'hawaiian', 'veggie']
+
+# Print the names of all the pizzas.
+for pizza in favorite_pizzas:
+    print(pizza)
+
+# Print a sentence about each pizza.
+for pizza in favorite_pizzas:
+    print(f"\nI really love {pizza} pizza!")
+
+print("\nI really love pizza!")
