e7293cc454a00b82caf039643781b6661f6fed45
ryanwilkinss <78103496+ryanwilkinss@users.noreply.github.com>
2021-02-04 09:45:34 -0800

    Create 3-4.py

---
+++ Do it Yourself/Chapter 3/3-4.py
@@ -0,0 +1,10 @@
+# 3-4. Guest List: If you could invite anyone, living or deceased, to dinner, who
+# would you invite? Make a list that includes at least three people you’d like to
+# invite to dinner. Then use your list to print a message to each person, inviting
+# them to dinner.
+
+guests = ['elliott', 'andreas', 'scotty']
+
+print(f"{guests[0].title()}, please come to dinner.")
+print(f"{guests[1].title()}, please come to dinner.")
+print(f"{guests[2].title()}, please come to dinner.")
