a9c329c8637d397c1d494fc492469c75e15483d7
ryanwilkinss <78103496+ryanwilkinss@users.noreply.github.com>
2021-02-03 20:20:13 -0800

    Update 2-4.py

--- Do it Yourself/Chapter 2/2-4.py
+++ Do it Yourself/Chapter 2/2-4.py
@@ -1,7 +1,7 @@
 # 2-4. Name Cases: Use a variable to represent a person’s name, and then print
 # that person’s name in lowercase, uppercase, and title case.
 
-name = "ryan"
+name = 'ryan'
 message = name.lower()
 print(message)
 message = name.upper()
