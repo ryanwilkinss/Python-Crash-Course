71c0811717c5df23ca7e35911051a2e7913f6627
ryanwilkinss <78103496+ryanwilkinss@users.noreply.github.com>
2021-02-03 20:08:48 -0800

    Create 2-5.py

---
+++ Do it Yourself/Chapter 2/2-5.py
@@ -0,0 +1,5 @@
+# 2-5. Famous Quote: Find a quote from a famous person you admire. Print the
+# quote and the name of its author. Your output should look something like the
+# following, including the quotation marks:
+
+print('Albert Einstein once said,\n“A person who never made a mistake never tried anything new.”')
