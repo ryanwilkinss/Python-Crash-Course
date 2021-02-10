52a176c256a5a399ec5ae504933448095932fdfa
ryanwilkinss <78103496+ryanwilkinss@users.noreply.github.com>
2021-02-09 17:58:37 -0800

    Create 5-5.py

---
+++ Do it Yourself/Chapter 5/5-5.py
@@ -0,0 +1,35 @@
+# Turn your if-else chain from Exercise 5-4 into an if-elif-else cahin.
+# If the alien is green, print a message that the player earned 5 points.
+# If the alien is yellow, print a message that the player earned 10 points.
+# If the alien is red, print a message that the player earned 15 points.
+# Write three versions of this program, making sure each message is printed for 
+# the appropriate color alien.
+# Version 1
+alien_color = 'green'
+
+if alien_color == 'green':
+    print("You just earned 5 points!")
+elif alien_color == 'yellow':
+    print("You just earned 10 points!")
+else:
+    print("You just earned 15 points!")
+
+# Version 2
+alien_color = 'yellow'
+
+if alien_color == 'green':
+    print("You just earned 5 points!")
+elif alien_color == 'yellow':
+    print("You just earned 10 points!")
+else:
+    print("You just earned 15 points!")
+
+# Version 3
+alien_color = 'red'
+
+if alien_color == 'green':
+    print("You just earned 5 points!")
+elif alien_color == 'yellow':
+    print("You just earned 10 points!")
+else:
+    print("You just earned 15 points!")
