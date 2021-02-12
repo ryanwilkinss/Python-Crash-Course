d9a0e3190bbd08a284bde8361f4d30fb981c2e67
ryanwilkinss <78103496+ryanwilkinss@users.noreply.github.com>
2021-02-12 15:17:01 -0800

    Create 8-5.py

---
+++ Do it Yourself/Chapter 8/8-5.py
@@ -0,0 +1,13 @@
+# Write a function called describe_city() that accepts the name of a city and 
+# its country. The function should print a simple sentence, such as Reykjavik 
+# is in Iceland. Give the parameter for the country a default value. Call your 
+# function for three different cities, at least one of which is not in the 
+# default country.
+def describe_city(city, country='chile'):
+    """Describe a city."""
+    msg = f"{city.title()} is in {country.title()}."
+    print(msg)
+
+describe_city('santiago')
+describe_city('reykjavik', 'iceland')
+describe_city('punta arenas')
