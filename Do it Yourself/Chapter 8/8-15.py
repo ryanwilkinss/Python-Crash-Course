a456ec9c6b3f80b1e6a30e0a9a7c59af21bea918
ryanwilkinss <78103496+ryanwilkinss@users.noreply.github.com>
2021-02-17 10:09:19 -0800

    Create 8-15.py

---
+++ Do it Yourself/Chapter 8/8-15.py
@@ -0,0 +1,32 @@
+# Put the functions for the example printing_models.py in a separate file 
+# called printing_functions.py. Write an import statement at the top of 
+# printing_models.py, and modify the file to use the imported functions.
+# printing_functions.py:
+"""Functions related to printing 3d models."""
+
+def print_models(unprinted_designs, completed_models):
+    """
+    Simulate printing each design, until there are none left.
+    Move each design to completed_models after printing.
+    """
+    while unprinted_designs:
+        current_design = unprinted_designs.pop()
+    
+        # Simulate creating a 3d print from the design.
+        print(f"Printing model: {current_design}")
+        completed_models.append(current_design)
+        
+def show_completed_models(completed_models):
+    """Show all the models that were printed."""
+    print("\nThe following models have been printed:")
+    for completed_model in completed_models:
+        print(completed_model)
+
+# printing_models.py:
+import printing_functions as pf
+
+unprinted_designs = ['iphone case', 'robot pendant', 'dodecahedron']
+completed_models = []
+
+pf.print_models(unprinted_designs, completed_models)
+pf.show_completed_models(completed_models)
