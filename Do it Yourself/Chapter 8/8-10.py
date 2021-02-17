9bfeca07462a48f71a60a47134d89d105e25a602
ryanwilkinss <78103496+ryanwilkinss@users.noreply.github.com>
2021-02-17 07:20:17 -0800

    Create 8-10.py

---
+++ Do it Yourself/Chapter 8/8-10.py
@@ -0,0 +1,27 @@
+# Start with a copy of your program from Exercise 8-9. Write a function called 
+# send_messages() that prints each text message and moves each message to a new 
+# list called sent_messages as it’s printed. After calling the function, print 
+# both of your lists to make sure the messages were moved correctly.
+def show_messages(messages):
+    """Print all messages in the list."""
+    print("Showing all messages:")
+    for message in messages:
+        print(message)
+
+def send_messages(messages, sent_messages):
+    """Print each message, and then move it to sent_messages."""
+    print("\nSending all messages:")
+    while messages:
+        current_message = messages.pop()
+        print(current_message)
+        sent_messages.append(current_message)
+
+messages = ["hello there", "how are u?", ":)"]
+show_messages(messages)
+
+sent_messages = []
+send_messages(messages, sent_messages)
+
+print("\nFinal lists:")
+print(messages)
+print(sent_messages)
